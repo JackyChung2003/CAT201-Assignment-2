2a7f5995bbadbf4f060c4f28cd5f772c1b5c6f6a
Jacky Chung <clashofjacky@email.com>
2023-12-19 23:03:43 +0800

    added website Navbar

--- quantum-charge-app/src/components/Navbar.js
+++ quantum-charge-app/src/components/Navbar.js
@@ -1,6 +1,35 @@
+// import logoImage from '../../assets/images/quantum-charge-logo.png';
+// import logoImage from './../assets/images/logo.png';
+import React from 'react';
+import './Navbar.css';
+
+
 function Navbar(){
     return(
-        <><h1>This is navbar</h1></>
+        <>
+            <nav>
+                <a href="index.html">
+                    <img 
+                        className="logo-img"
+                        src="assets/images/quantum-charge-logo.png" 
+                        alt="QuantumCharge Logo" 
+                    />
+                </a>
+
+                <div>
+                    <ul id="navbar">
+                        <li><a className="active" href="index.html">Home</a></li>
+                        <li><a href="populars.html">Populars</a></li>
+                        <li><a href="new-releases.html">New Releases</a></li>
+                        <li><a href="why-us.html">Why Us</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                    </ul>
+
+
+                </div>
+            </nav>
+        </>
     )
 }
 
