6750fed64c56d0ff3e06c586e7d169b5fbfe2e5a
Jacky Chung <clashofjacky@email.com>
2023-12-20 00:18:48 +0800

    Mobile responsive Navar added

--- quantum-charge-app/src/components/Navbar.js
+++ quantum-charge-app/src/components/Navbar.js
@@ -1,36 +1,48 @@
 // import logoImage from '../../assets/images/quantum-charge-logo.png';
 // import logoImage from './../assets/images/logo.png';
-import React from 'react';
+import React, { Component } from 'react';
 import './Navbar.css';
 
 
-function Navbar(){
-    return(
-        <>
-            <nav>
-                <a href="index.html">
-                    <img 
-                        className="logo-img"
-                        src="assets/images/quantum-charge-logo.png" 
-                        alt="QuantumCharge Logo" 
-                    />
-                </a>
+class Navbar extends Component{
+    state = {clicked: false};
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
 
-                <div>
-                    <ul id="navbar">
-                        <li><a className="active" href="index.html">Home</a></li>
-                        <li><a href="populars.html">Populars</a></li>
-                        <li><a href="new-releases.html">New Releases</a></li>
-                        <li><a href="why-us.html">Why Us</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
-                    </ul>
+    render(){
+        return(
+            <>
+                <nav>
+                    <a href="index.html">
+                        <img 
+                            className="logo-img"
+                            src="assets/images/quantum-charge-logo.png" 
+                            alt="QuantumCharge Logo" 
+                        />
+                    </a>
+
+                    <div>
+                        <ul id="navbar" className={this.state.clicked ? "navbar active" : "#navbar"}>
+                            <li><a className="active" href="index.html">Home</a></li>
+                            <li><a href="populars.html">Populars</a></li>
+                            <li><a href="new-releases.html">New Releases</a></li>
+                            <li><a href="why-us.html">Why Us</a></li>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
 
 
-                </div>
-            </nav>
-        </>
-    )
+                    </div>
+
+                    <div id="mobile" onClick={this.handleClick}>
+                        <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                    </div>
+
+                </nav>
+            </>
+        )
+    }
 }
 
 export default Navbar;
