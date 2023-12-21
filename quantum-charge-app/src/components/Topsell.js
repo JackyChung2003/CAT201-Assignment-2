5e8866f5dc2faf09ce5b90e59a76a76c5aabe3d1
Jacky Chung <clashofjacky@email.com>
2023-12-21 20:52:21 +0800

    added flip card code

--- quantum-charge-app/src/components/Topsell.js
+++ quantum-charge-app/src/components/Topsell.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Topsell.css';
+import Powerbank1 from '../assets/images/powerbank_1.jpeg';
 
 const Topsell = () => {
     const [isFlipped, setFlipped] = useState(false);
@@ -14,16 +15,27 @@ const Topsell = () => {
               <div className="flip-card-inner" style={{ transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0)' }}>
                 <div className="flip-card-front">
                     <div className='product-image'>
-                        [picture]
+                        <img src={Powerbank1} alt='Powerbank1' />
                     </div>
                     <div className='product-info'>
-                        <h1>Product Name</h1>
-                        <p>Product Description</p>
-                        <p>Product Price</p>
+                        <h1>BEST SALLER of the year</h1>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <h1>Quantum S8 Ultra</h1>
+                        <p>A powerhouse of innovation and style. Experience rapid charging and futuristic design, setting a new standard in portable energy.</p>
+                        <p>T&C Apply.</p>
+
+                        <div className='button-container'>  
+                          <button className="flip-button" onClick={handleFlip}>
+                            <span class="hover-underline-animation"> View detail </span>
+                          </button>
+                          <button className="buy-button" onClick={handleFlip}>
+                            <span>Buy Now</span>
+                          </button>
+                        </div>
                         
-                        <button className="flip-button" onClick={handleFlip}>
-                          Flip Card
-                        </button>
                     </div>
                   {/* <p className="title">FLIP CARD</p>
                   <p>Hover Me</p> */}
